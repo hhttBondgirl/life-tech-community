5ebd16a67b67ed82493b425e32bc0eef699deded
Naomi <mukade.tiger1111@gmail.com>
2026-03-15 20:09:21 +0900

    Fix: Add home route and restore posts.index

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 
-
+// 1. ブラウザの「Back」ボタンなどで使っている名前
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
-// Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+// 2. GitHub Actionsや認証機能が求めている名前（中身は上と同じ）
+Route::get('/home', [PostController::class, 'index'])->name('home');
 
 Route::resource('posts', PostController::class)->except(['index']);
 Route::resource('posts.comments', CommentController::class)->only(['store', 'destroy']);
