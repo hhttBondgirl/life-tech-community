5b0f1a27489f13789b4c5ce933fbc073bda0ccec
Naomi <mukade.tiger1111@gmail.com>
2026-03-28 15:29:24 +0900

    Fix: add Vite to layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title }}</title>
-    
+    <title>{{ $title ?? config('app.name') }}</title>
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ url('style.css') }}">
+    {{-- ViteでビルドされたCSS/JSを読み込む（本番でも正しく読みます） --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div class="container">
